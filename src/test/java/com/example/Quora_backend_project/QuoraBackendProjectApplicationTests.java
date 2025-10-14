3c880f2fd78b1756c7ef3748dd82f0343317f032
vivektarun <vivektarun1234@gmail.com>
2025-10-14 18:17:59 +0530

    config: configured .env setup

---
+++ src/test/java/com/example/Quora_backend_project/QuoraBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quora_backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoraBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
